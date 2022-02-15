b602c18b71269754ed1b3bdaba648052221ec989
Rizzzi Git <rizzzigit@gmail.com>
2022-02-15 19:39:30 +0800

    logger

--- dist/core/client.js
+++ dist/core/client.js
@@ -11,10 +11,10 @@ var Client = /** @class */ (function () {
         this.discordClient = client;
         this.database = new query_builder_1.QueryBuilder(this, databaseCredentials);
         this.events = new events_1.EventEmitter(this);
+        this.logger = new logger_1.ScopedLogger(new logger_1.Logger(this), 'main');
         this.modules = new module_1.ModuleManager(this);
         this.on = this.events.on.bind(this.events);
         this.once = this.events.once.bind(this.events);
-        this.logger = new logger_1.ScopedLogger(new logger_1.Logger(this), 'main');
     }
     Client.defaultOptions = {};
     return Client;
